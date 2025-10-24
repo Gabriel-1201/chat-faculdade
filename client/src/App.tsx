fc84bf768bcaf1d9c80d700a2537b2c7b01a233e
Gabriel-1201 <pessoal@gabrielataide.com>
2025-10-24 19:52:37 -0300

    Frontend épico

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,6 +6,10 @@ export default function App() {
   const [message, setMessage] = useState<string>();
   const [chat, setChat] = useState<any>([]);
 
+    const [kb_toDo, setToDo] = useState<any>([]);
+    const [kb_doing, setDoing] = useState<any>([]);
+    const [kb_done, setDOne] = useState<any>([]);
+
   useEffect(() => {
     socket.on("connect", () => {
       console.log('Conectado ao servidor');
@@ -29,23 +33,38 @@ export default function App() {
 
   return (
     <div className="p-6 px-48">
-      <h3 className="font-semibold text-2xl mb-4">Chat em Tempo Real</h3>
-      <div className='flex flex-row items-center space-x-4'>
-        <label htmlFor="for-username">Nome</label>
-        <input
-          className="border p-1 rounded w-96" 
-          type="text" 
-          placeholder="Seu nome..."
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-        />
-      </div>
-      <div className="border h-[300px] overflow-y-auto p-4 mt-2 bg-gray-100">
-        {
-          chat.map((msg: any, i: number) => (
-            <p key={i}><strong>{msg.username}: {msg.message}</strong></p>
-          ))
-        }
+      <h3 className="font-semibold text-2xl mb-4">Kanban</h3>
+      <div className='flex flex-row items-center space-x-4 mt-4'>
+        <div>
+          <label htmlFor="for-username">ToDo</label>
+          <div className="border h-[300px] w-[200px] overflow-y-auto p-4 mt-2 bg-gray-100">
+            {
+              kb_toDo.map((msg: any, i: number) => (
+                <p key={i}><strong>{msg.username}: {msg.message}</strong></p>
+              ))
+            }
+          </div>
+        </div>
+        <div>
+          <label htmlFor="for-username">Doing</label>
+          <div className="border h-[300px] w-[200px] overflow-y-auto p-4 mt-2 bg-gray-100">
+            {
+              kb_doing.map((msg: any, i: number) => (
+                <p key={i}><strong>{msg.username}: {msg.message}</strong></p>
+              ))
+            }
+          </div>
+        </div>
+        <div>
+          <label htmlFor="for-username">Done</label>
+          <div className="border h-[300px] w-[200px] overflow-y-auto p-4 mt-2 bg-gray-100">
+            {
+              kb_done.map((msg: any, i: number) => (
+                <p key={i}><strong>{msg.username}: {msg.message}</strong></p>
+              ))
+            }
+          </div>
+        </div>
       </div>
 
       <form onSubmit={sendMessage}>
